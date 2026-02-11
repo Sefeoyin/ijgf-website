f48c43acd35281a5c091f872a185127fb3f3f60b
sefeoyin <106766750+Sefeoyin@users.noreply.github.com>
2026-02-11 08:53:00 +0100

    Fix scroll to top on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence, motion } from 'framer-motion'
+import { useEffect } from 'react'
 import Navigation from './Navigation'
 import Footer from './Footer'
 import LandingPage from './LandingPage'
 import WaitlistPage from './WaitlistPage'
 import ShareStoryPage from './ShareStoryPage'
 import './App.css'
+
+// Scroll to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 // Animated page wrapper component
 function AnimatedRoutes() {
@@ -84,6 +96,7 @@ function AnimatedRoutes() {
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Navigation />
         <AnimatedRoutes />
