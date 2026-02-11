bc5c68c90b24fd63576caaf669b52aebf88e4364
sefeoyin <106766750+Sefeoyin@users.noreply.github.com>
2026-02-11 17:46:13 +0100

    fix hero page objectposition

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -65,7 +65,7 @@ function LandingPage() {
               width: '100%',
               height: '100%',
               objectFit: 'cover',
-              objectPosition: 'center 45%',
+              objectPosition: 'center 15%',
               margin: 0,
               padding: 0,
             }}
